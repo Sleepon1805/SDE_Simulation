8be1f9e0aa723000752b8501a581c5167f9b298a
Sleepon1805 <maxikon1805@gmail.com>
2024-01-07 15:59:17 +0100

    implemented batched simulation

--- sde_simulation.py
+++ sde_simulation.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import matplotlib.pyplot as plt
 from time import time
@@ -6,7 +7,8 @@ from sklearn.linear_model import LinearRegression
 
 from sde_lib import SDE, GeometricBrownianMotion, OrnsteinUhlenbeck, CoxIngersollRoss
 from approximation_methods import get_approximation_method, APPROXIMATION_METHODS
-from utils import on_device, SEED
+
+DEFAULT_BATCH_SIZE = 10000  # 20000 (num_simulations) x 32768 (timesteps) still pass on my Radeon RX 6700 XT
 
 
 class ConvergenceRateCalculator:
@@ -20,19 +22,28 @@ class ConvergenceRateCalculator:
 
         self.num_steps_grid = [int(sde.T / dt) for dt in dt_grid]
 
+        # for gpu calculations
+        self.batch_size = min(DEFAULT_BATCH_SIZE, num_simulations)
+
+        # for plotting
+        self.colors = ['blue', 'orange', 'green']
+        self.linestyles = ['-', '--', ':']
+
     def test_different_sde_params(self, param_name: str, param_values: List[float], use_exact_solution: True, visualize=True):
         convergence_rates = {}
         for param_value in param_values:
             print(f"Testing {param_name} = {param_value}")
             setattr(self.sde, param_name, param_value)
             convergence_rates[param_value] = self.calculate_convergence_rates(use_exact_solution, visualize)
+
         if visualize:
-            for method_name, p in convergence_rates[param_values[0]].keys():
-                plt.plot(
-                    param_values,
-                    [convergence_rates[param_value][(method_name, p)] for param_value in param_values],
-                    label=f"{method_name} - p = {p}"
-                )
+            for i, method_name in enumerate(self.approximation_methods):
+                for j, p in enumerate(self.p_values):
+                    plt.plot(
+                        param_values,
+                        [convergence_rates[param_value][(method_name, p)] for param_value in param_values],
+                        label=f"{method_name} - p = {p}", color=self.colors[i], ls=self.linestyles[j]
+                    )
             plt.title(f'{"Pseudo " if not use_exact_solution else ""}Approximation Errors for\n {self.sde}')
             plt.xlabel(param_name)
             plt.ylabel('Convergence Rate')
@@ -41,74 +52,79 @@ class ConvergenceRateCalculator:
         return convergence_rates
 
     def calculate_convergence_rates(self, use_exact_solution: True, visualize=True):
-        adapted_sde_name = str(self.sde).replace(f"N={self.sde.N}", f"N in {self.num_steps_grid}")
+        adapted_sde_name = str(self.sde).replace(
+            f"N={self.sde.N}",
+            f"N=[{max(self.num_steps_grid)} ... {max(self.num_steps_grid)}]"
+        )
         print(f'SDE: {adapted_sde_name}')
 
-        errors = self._calculate_errors_for_sde(use_exact_solution, visualize)
-
-        convergence_rates = self._calculate_convergence_rates_from_errors(errors)
-
-        return convergence_rates
-
-    def _calculate_errors_for_sde(self, use_exact_solution: True, visualize=True) -> Dict[Tuple[str, int], List[float]]:
-
-        errors = {(method_name, p): [] for method_name in APPROXIMATION_METHODS for p in self.p_values}
-
-        # sample brownian motion with the highest discretization
-        highest_res_N = max(self.num_steps_grid) * (2 ** 2)
-        highest_res_sde = self.sde.copy()
-        highest_res_sde.update_time_discretization(num_steps=highest_res_N)
-        brownian_motion = highest_res_sde.sample_brownian_motion(num_simulations=self.num_simulations, seed=SEED)
-
-        if not use_exact_solution:
-            print(f"Simulating pseudo exact solutions with N = {highest_res_N}", end="")
-            ttime = time()
-            pseudo_exact_solutions = {
-                method_name: get_approximation_method(method_name)(highest_res_sde, brownian_motion)
-                for method_name in APPROXIMATION_METHODS
-            }
-            print(f" ({time() - ttime:.2f}s)")
-
-        for N in self.num_steps_grid:
-            print(f"Running simulations for N = {N}", end="")
-            ttime = time()
-
-            self.sde.update_time_discretization(num_steps=N)
-            reduced_brownian_motion = brownian_motion[::int(highest_res_N/N)]
-
-            if use_exact_solution:
-                exact_solutions = self.sde.exact_solutions(reduced_brownian_motion)
-                exact_solutions = {
-                    method_name: exact_solutions for method_name in APPROXIMATION_METHODS
-                }
-            else:
-                exact_solutions = {
-                    method_name: pseudo_exact_solutions[method_name][::int(highest_res_N/N)]
-                    for method_name in APPROXIMATION_METHODS
-                }
-
-            for method_name in APPROXIMATION_METHODS:
-                approximations = get_approximation_method(method_name)(self.sde, reduced_brownian_motion)
-                for p in self.p_values:
-                    errors[(method_name, p)].append(
-                        self._calculate_lp_error(exact_solutions[method_name], approximations, p=p)
-                    )
-
-            print(f" ({time() - ttime:.2f}s)")
+        errors = []
+        num_batches = self.num_simulations // self.batch_size
+        ttime = time()
+        for batch_num in range(num_batches):
+            print(f'Batch {batch_num + 1}/{num_batches}')
+            errors.append(self._calculate_errors(self.batch_size, use_exact_solution))
+        errors = {key: sum([error[key] for error in errors]) / num_batches for key in errors[0].keys()}
+        print(f"Total simulation time: {time() - ttime:.2f}s")
 
         if visualize:
             # plot lp errors
-            colors = ['blue', 'orange', 'green']
-            linestyles = ['-', '--', ':']
-            for i, method_name in enumerate(APPROXIMATION_METHODS):
+            for i, method_name in enumerate(self.approximation_methods):
                 for j, p in enumerate(self.p_values):
-                    plt.loglog(self.dt_grid, errors[(method_name, p)], label=f"{method_name} - p = {p}",
-                               ls=linestyles[j], color=colors[i])
+                    plt.loglog(self.dt_grid, [errors[(method_name, p, N)] for N in self.num_steps_grid],
+                               label=f"{method_name} - p = {p}", color=self.colors[i], ls=self.linestyles[j])
             plt.title(f'{"Pseudo " if not use_exact_solution else ""}Approximation Errors for\n {self.sde}')
             plt.xlabel('$\Delta t$')
             plt.ylabel('Error (e($\Delta t$))')
             plt.legend()
             plt.show()
+
+        convergence_rates = self._calculate_convergence_rates_from_errors(errors)
+
+        return convergence_rates
+
+    def _calculate_errors(self, num_simulations: int, use_exact_solution: bool) -> Dict[Tuple[str, int, int], float]:
+        errors = {
+            (method_name, p, N): 0
+            for method_name in self.approximation_methods for p in self.p_values for N in self.num_steps_grid
+        }
+
+        # sample brownian motion with the highest discretization
+        highest_res_N = max(self.num_steps_grid) * (2 ** 2)
+        highest_res_sde = self.sde.copy()
+        highest_res_sde.update_time_discretization(num_steps=highest_res_N)
+        brownian_motion = highest_res_sde.sample_brownian_motion(num_simulations=num_simulations)
+
+        # calculate exact solutions as approximations with higher discretization
+        if use_exact_solution:
+            print(f"Simulating exact solutions with N = {highest_res_N}", end="")
+            ttime = time()
+            all_exact_solutions = highest_res_sde.exact_solutions(brownian_motion)
+            all_exact_solutions = {
+                method_name: all_exact_solutions for method_name in self.approximation_methods
+            }
+            print(f" ({time() - ttime:.2f}s)")
+        else:
+            print(f"Simulating pseudo exact solutions for N = {highest_res_N}", end="")
+            ttime = time()
+            all_exact_solutions = {}
+            for method_name in self.approximation_methods:
+                all_exact_solutions[method_name] = get_approximation_method(method_name)(
+                    sde=highest_res_sde, W=brownian_motion)
+            print(f" ({time() - ttime:.2f}s)")
+
+        print(f"Running simulations for N in {self.num_steps_grid}:")
+        for N in self.num_steps_grid:
+            ttime = time()
+            self.sde.update_time_discretization(num_steps=N)
+            for method_name in self.approximation_methods:
+                exact_solutions = all_exact_solutions[method_name][::int(highest_res_N/N)]
+                reduced_brownian_motion = brownian_motion[::int(highest_res_N/N)]
+                approximations = get_approximation_method(method_name)(self.sde, reduced_brownian_motion)
+                for p in self.p_values:
+                    errors[(method_name, p, N)] += self._calculate_lp_error(
+                        exact_solutions, approximations, p=p)
+            print(f"{time() - ttime:.2f}s + ", end="")
         return errors
 
     @staticmethod
@@ -122,20 +138,18 @@ class ConvergenceRateCalculator:
         """
         assert p >= 1, "p \in [1, \infty)"
         error = torch.max(torch.mean(torch.abs(solutions - calc_approximations) ** p, dim=1) ** (1/p))
-        return error.cpu()
+        return error.cpu().item()
 
     def _calculate_convergence_rates_from_errors(self, error_values):
         """
         Calculate convergence rate from given errors and time discretizations.
-        :param error_values: torch.Tensor: errors of shape (num_dts). $Err(\Delta t)$ above
-        :return: float: convergence rate
         """
         print("---")
         convergence_rates = {}
-        for method_name in APPROXIMATION_METHODS:
+        for method_name in self.approximation_methods:
             for p in self.p_values:
                 x = torch.log(torch.tensor(self.dt_grid)).reshape(-1, 1)
-                y = torch.log(torch.tensor(error_values[(method_name, p)]))
+                y = torch.log(torch.tensor([error_values[(method_name, p, N)] for N in self.num_steps_grid]))
                 reg = LinearRegression().fit(x, y)
                 convergence_rates[(method_name, p)] = reg.coef_.item()
                 print(f"{method_name} convergence rate for p = {p}: {convergence_rates[(method_name, p)]}")
@@ -153,8 +167,8 @@ if __name__ == '__main__':
         sde=SDE,
         approximation_methods=APPROXIMATION_METHODS,
         dt_grid=[2 ** i for i in range(-13, -4)],
-        p_values=[1],
-        num_simulations=20000,
+        p_values=[1, 2],
+        num_simulations=100000,
     )
     # calc.calculate_convergence_rates(use_exact_solution=False, visualize=True)
-    calc.test_different_sde_params('a', [0.01, 0.1, 0.25, 0.5, 1, 2, 4], use_exact_solution=False, visualize=True)
+    calc.test_different_sde_params('a', [0.1, 0.25, 0.5, 1, 2, 4], use_exact_solution=False, visualize=True)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+from tqdm import tqdm
 
 SEED = 137
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -14,3 +15,20 @@ def on_device(func):
         kwargs = {k: v.to('cpu') if hasattr(v, 'to') else v for k, v in kwargs.items()}
         return result.to('cpu')
     return wrapper
+
+
+def run_batched(func, arg_name, batch_size, batch_dim, *args, **kwargs):
+    assert arg_name in kwargs, f'Argument {arg_name} not found in kwargs'
+    assert isinstance(kwargs[arg_name], torch.Tensor), f'Argument {arg_name} is not a torch.Tensor'
+    arg = kwargs[arg_name]
+
+    if arg.shape[batch_dim] <= batch_size:
+        return func(*args, **kwargs)
+
+    arg_batched = torch.split(arg, batch_size, dim=batch_dim)
+    results = []
+    for arg_batch in tqdm(arg_batched):
+        kwargs[arg_name] = arg_batch
+        results.append(func(*args, **kwargs))
+    results = torch.cat(results, dim=batch_dim)
+    return results
